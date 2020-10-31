be3939c4df8211c524c1c52fb4f80c19f91ac1b8
Anna Taylor <annatnyc@gmail.com>
2020-10-30 22:57:51 -0400

    Extending features to allow users to access more information

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Anna Taylor on 10/30/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -25,7 +25,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource,
         
 
         // Do any additional setup after loading the view.
-        print("I triggered the network request")
+        
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -79,14 +79,29 @@ class MoviesViewController: UIViewController, UITableViewDataSource,
         return cell
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        print("Loading up the details screen")
+        
+        //Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie to the details view controller
+        
+        let detailsViewController = segue.destination
+        as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
